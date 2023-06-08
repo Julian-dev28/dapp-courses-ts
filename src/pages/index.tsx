2438323fdf3260b57f46191623702a5c3bd421b3
Julian Martinez <73849597+Julian-dev28@users.noreply.github.com>
2023-06-08 14:27:13 -0700

    update landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,6 +36,79 @@ const Landing: React.FC = () => {
   );
 };
 
+const GettingStartedCard = ({ index, title, subtitle, href }) => (
+  <a className="GettingStartedCard" href={href}>
+    {index && <p className="GettingStartedCard__index">{index}</p>}
+
+    <p className="GettingStartedCard__title">{title}</p>
+
+    <p className="GettingStartedCard__subtitle">{subtitle}</p>
+
+    <div className="GettingStartedCard__icon">
+      <img
+        className="GettingStartedCard__unactive"
+        src="/img/landing/arrow-black.svg"
+        alt="arrow"
+      />
+      <img
+        className="GettingStartedCard__active"
+        src="/img/landing/arrow-white.svg"
+        alt="arrow"
+      />
+    </div>
+  </a>
+);
+
+const GettingStarted = () => (
+  <div className="GettingStarted">
+    <div className="hp-center">
+      <div className="GettingStarted__cards">
+        <GettingStartedCard
+          title="Build a Crowdfunding Dapp"
+          subtitle="Create a decentralized application for crowdfunding projects."
+          href="/docs/category/dapp-challenges"
+          index={undefined}
+        />
+
+        <GettingStartedCard
+          title="Build an AMM"
+          subtitle="Develop an Automated Market Maker for decentralized exchanges."
+          href="/docs/category/dapp-challenges"
+          index={undefined}
+        />
+
+        <GettingStartedCard
+          title="Build an Oracle"
+          subtitle="Implement an Oracle service for accessing external data on the blockchain."
+          href="/docs/category/dapp-challenges"
+          index={undefined}
+        />
+
+        <GettingStartedCard
+          title="Build an NFT Gallery"
+          subtitle="Construct a platform to showcase and trade Non-Fungible Tokens."
+          href="/docs/category/dapp-challenges"
+          index={undefined}
+        />
+
+        <GettingStartedCard
+          title="Build a Multisig Wallet"
+          subtitle="Create a wallet that requires multiple signatures for transactions."
+          href="/docs/category/dapp-challenges"
+          index={undefined}
+        />
+
+        <GettingStartedCard
+          title="Build a Smart Wallet"
+          subtitle="Develop a wallet with programmable and automated functionality."
+          href="/docs/category/dapp-challenges"
+          index={undefined}
+        />
+      </div>
+    </div>
+  </div>
+);
+
 const Hero: React.FC = () => (
   <div className="Hero">
     <div className="Hero__wrapper hp-center">
@@ -49,13 +122,34 @@ const Hero: React.FC = () => (
           <div>Soroban</div>
         </div>
       </div>
+      <div className="Hero__right">
+        <p>
+          Jumpstart your Dapp development with Soroban. Soroban is a smart
+          contracts platform that is designed with purpose and built to perform.
+          Futurenet is Live! Start experimenting now!
+        </p>
+
+        <div className="Hero__btn-group">
+          <a
+            href="/docs/category/dapp-challenges"
+            className="hp-btn hp-btn-primary"
+          >
+            Challenges
+          </a>
+        </div>
+      </div>
     </div>
+    <img className="Hero__img1" src="/img/landing/image-5.svg" alt="1" />
+    <img className="Hero__img2" src="/img/landing/image-5.svg" alt="2" />
+    <img className="Hero__img3" src="/img/landing/image-3.svg" alt="3" />
+
+    <img className="Hero__img5" src="/img/landing/image-5.svg" alt="5" />
   </div>
 );
 
 const Designed: React.FC = () => <div />;
 const Learn: React.FC = () => <div />;
-const GettingStarted: React.FC = () => <div />;
+
 const Earn: React.FC = () => <div />;
 const SCF: React.FC = () => <div />;
 const Soroban: React.FC = () => <div />;
