41afb9019df8e7d49855417690718c3ec5b4b4bb
Julian Martinez <73849597+Julian-dev28@users.noreply.github.com>
2023-06-15 15:26:17 -0700

    add course deployments tracking

--- src/components/atoms/courses-card/index.tsx
+++ src/components/atoms/courses-card/index.tsx
@@ -7,19 +7,21 @@ interface CardProps {
 
 interface Course {
   publickey: string;
-  url: string;
+  url: string[];
 }
 
 const fetchCourses = async (address: string): Promise<string[]> => {
   try {
     const response = await fetch(
       "https://dapp-wrangler.julian-martinez.workers.dev/"
-    ); // replace with your
+    );
     const data: Course[] = await response.json();
 
-    return data
-      .filter((course) => course.publickey === address)
-      .map((course) => course.url);
+    const filteredCourses = data.filter(
+      (course) => course.publickey === address
+    );
+
+    return filteredCourses.flatMap((course) => course.url);
   } catch (error) {
     console.error(error);
     return [];
@@ -27,11 +29,11 @@ const fetchCourses = async (address: string): Promise<string[]> => {
 };
 
 export function DeployedProjectsCard({ addressHex }: CardProps) {
-  const [completedCourses, setCompletedCourses] = useState<string[]>([]);
+  const [deployments, setDeployments] = useState<string[]>([]);
 
   useEffect(() => {
     if (addressHex) {
-      fetchCourses(addressHex).then((courses) => setCompletedCourses(courses));
+      fetchCourses(addressHex).then((courses) => setDeployments(courses));
     }
   }, [addressHex]);
 
@@ -39,8 +41,8 @@ export function DeployedProjectsCard({ addressHex }: CardProps) {
     <div className={styles.card}>
       <h3>Dapp Deployments</h3>
       <ul>
-        {completedCourses.map((course, index) => (
-          <li key={index}>{course}</li>
+        {deployments.map((deployment, index) => (
+          <li key={index}>{deployment}</li>
         ))}
       </ul>
     </div>
